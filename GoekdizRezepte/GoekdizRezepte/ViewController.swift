afb5c09773394bb1ac0c62d24d601589c8ca16a5
dddasd <asdasdasd>
2021-09-22 10:42:18 +0200

    Projekt angelegt.

---
+++ GoekdizRezepte/GoekdizRezepte/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoekdizRezepte
+//
+//  Created by Hakan Yildiz / BBS2H20A on 22.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
